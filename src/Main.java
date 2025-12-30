58b6670abfa305bffde4f0178cf8d406574e04f7
student7 <codecat.ua@gmail.com>
2025-12-30 19:21:03 +0200

    all

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        User Andrey = new User(835162780,"Andrey","Sanerko",1000);
+
+    }
+}
--- src/User.java
+++ src/User.java
@@ -2,5 +2,12 @@ public class User {
     public int ID;
     public String firsName;
     public String lastName;
+    public int sum;
 
+    User (int ID,String firsName,String lastName,int sum){
+        this.ID = ID;
+        this.firsName = firsName;
+        this.lastName = lastName;
+        this.sum = sum;
+    }
 }
