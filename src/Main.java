f4dbed376dc4d7338da5b2db3a5929df7ca9db3f
student7 <codecat.ua@gmail.com>
2025-12-30 19:32:15 +0200

    new objects

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         User Andrey = new User(835162780,"Andrey","Sanerko",1000);
+        User Dmytro = new User(835182780,"Dmytro","raterko",1960);
+        User Vasya = new User(845162780,"Vasya","kanerto",9000);
+        User Kostya = new User(835162080,"Kostya","defuzenko",1000);
+
 
     }
 }
